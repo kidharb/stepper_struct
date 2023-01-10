5a9a1778460d0e70d5113a5fc9ddb7d168fb50e6
Kidhar Bachan <kidharb@gmail.com>
2023-01-10 21:23:31 +0200

    Getting closer

--- stepper_struct.py
+++ stepper_struct.py
@@ -4,26 +4,34 @@ NUM_STEPPERS = 8
 MAX_STEPS = 20
 
 def clockOut():
-    bitstream = column(control_sequence,1)
-    print(bitstream)
+    global control_sequence
+    control_sequence = np.roll(control_sequence, -1, axis=1) 
+    control_sequence[:,-1] = 0
+
 
 def stepMotor(motor,steps,direction):
     global control_sequence
+
+    if (np.sum(control_sequence[2*motor])):
+        print("Motor", motor ,"is busy, please try again later")
+        return 1
 
     control_sequence[2*motor,1:2*steps:2] = 1
     control_sequence[2*motor+1,:2*steps:1] = [direction]
 
 
 
+
 signal, stepper = (1, 2*NUM_STEPPERS)
-control_sequence = np.array([[0] * MAX_STEPS * signal]*stepper)
+control_sequence = np.zeros((2*NUM_STEPPERS,MAX_STEPS))
 
 stepMotor(5,5,1)
 stepMotor(1,10,1)
 stepMotor(7,8,1)
-clockOut()
-#print(control_sequence)
+stepMotor(5,5,1)
 
+print(control_sequence)
 
-
-#printControlSequence(control_sequence)
+#for i in range(10):
+#    clockOut()
+print(control_sequence[:,1])
