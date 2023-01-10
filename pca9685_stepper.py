24f96e5c2e148d12464e38417582a727c3a435bd
Kidhar Bachan <kidharb@gmail.com>
2023-01-10 21:59:22 +0200

    This is working now

--- pca9685_stepper.py
+++ pca9685_stepper.py
@@ -4,6 +4,9 @@ from threading import Timer
 from board import SCL, SDA
 import busio
 
+import signal
+import time
+
 # Import the PCA9685 module.
 from adafruit_pca9685 import PCA9685
 
@@ -12,10 +15,11 @@ PCA9685_FREQUENCY=1800
 NUM_MOTORS=8
 
 # target task function
-def stopMotor(motor):
+def stopMotor0(signum, frame):
     # report the custom message
-    pca.channels[motor*2].duty_cycle = 0x0
-    print("stopMotor: Stopping motor",motor)
+    pca.channels[0*2].duty_cycle = 0x0
+    print("stopMotor: Stopping motor",0)
+
         
 def calcDuration(freq, steps):
     return (1/freq * steps)
@@ -24,13 +28,17 @@ def stepMotor(motor, freq, steps, direction):
    print("stepMotor: New request for Motor #",motor," Steps =",steps," Direction =",direction)
 
    duration = calcDuration(freq,steps)
-   print("On Duration is ",duration,"s")
+   print("On Duration is",duration,"s")
    pca.channels[motor+1].duty_cycle = direction
-   timer = Timer(duration, stopMotor, args=(motor,))
+   #timer = Timer(duration, stopMotor, args=(motor,))
 
-   timer.start()
+   #timer.start()
+   signal.signal(signal.SIGALRM, stopMotor0)
+
    # Turn on the PWM output for this motor
    pca.channels[motor*2].duty_cycle = 0x7FFF
+   signal.setitimer(signal.ITIMER_REAL, 0.000001, 0.1)
+
 
 if __name__ == "__main__":
     # Create the I2C bus interface.
@@ -41,7 +49,11 @@ if __name__ == "__main__":
     # Set the PWM frequency to 40hz.
     pca.frequency = PCA9685_FREQUENCY
     for i in range(0,NUM_MOTORS*2,2):
-        print("Init: Switching of PWM on PCA9685 output",i)
+        #print("Init: Switching off PWM on PCA9685 output",i)
         pca.channels[i].duty_cycle = 0x0
         
-    stepMotor(0,PCA9685_FREQUENCY,20,0xFFFF)
+    stepMotor(0,PCA9685_FREQUENCY,1,0xFFFF)
+    time.sleep(1)
+    signal.setitimer(signal.ITIMER_REAL, 0) # Disable the alarm
+
+
--- stepper_controller.py
+++ stepper_controller.py
@@ -2,6 +2,13 @@ from pi74HC595_stepper import pi74HC595_stepper
 import time
 
 stepper = pi74HC595_stepper()
+
+stepper.stepMotor(5,5,1)
+stepper.stepMotor(1,10,1)
+stepper.stepMotor(7,8,1)
+
 stepper.enable_timer(latch_every = 0.1)
+
+
 time.sleep(1)
 stepper.disable_timer()
